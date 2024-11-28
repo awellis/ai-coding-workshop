aa19a8fa413e1ebcc46496413fee5b0f78393040
Andrew Ellis <a.w.ellis@gmail.com>
2024-11-28 12:46:46 +0100

    add cell delimeters

--- test-openai.py
+++ test-openai.py
@@ -1,6 +1,8 @@
+#%%
 from dotenv import load_dotenv
 from openai import OpenAI 
 
+#%%
 # The load_dotenv() function from the python-dotenv library
 # loads environment variables from a .env file into the
 # current process's environment variables. This allows us
@@ -8,17 +10,18 @@ from openai import OpenAI
 # file that is not committed to version control.
 load_dotenv()
 
+#%%
 # Create a client object that will be used to interact with the OpenAI API.
 client = OpenAI()
 
-
+#%%
 response = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[{"role": "system", "content": "You are a helpful assistant."}, 
               {"role": "user", "content": "What is the weather in Bern?"}]
 )
-
+#%%
 print(response)
 
-
+#%%
 print(response.choices[0].message.content)
